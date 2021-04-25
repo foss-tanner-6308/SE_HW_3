43036911c98317e4c0d560cfc166c93666f1af8e
Tanner <fossta@oregonstate.edu>
2021-04-25 14:11:25 -0700

    added error handling

--- leapyear.py
+++ leapyear.py
@@ -1,12 +1,16 @@
-year = input("Enter a year: ")
+userInput = input("Enter a year: ")
 
-if (year % 4) == 0:
-    if (year % 100) == 0:
-        if (year % 400) == 0:
-            print(year, "is a leap year")
+try:
+    year = int(userInput)
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print(year, "is a leap year")
+            else:
+                print(year, "is not a leap year")
         else:
-            print(year, "is not a leap year")
+            print(year, "is a leap year")
     else:
-        print(year, "is a leap year")
-else:
-    print(year, "is not a leap year")
+        print(year, "is not a leap year")
+except ValueError:
+    print("That's not a year.")
